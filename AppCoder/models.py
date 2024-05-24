f02c7676bab1e9ade66950887564532245a8ff35
karla kumako <karlakumako@gmail.com>
2024-05-24 17:42:29 -0300

    se agregan las clases como tablas

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -6,13 +6,13 @@ class Curso (models.Model):
 
 class Estudiante (models.Model):
     nombre=models.CharField(max_length=30)
-    apellido=models.CharField(max_length=30)
-    email=models.EmailField()
+    Apellido=models.CharField(max_length=30)
+    email=models.EmailField(null=True)
 
 class Profesor(models.Model):
     nombre=models.CharField(max_length=30)
     apellido=models.CharField(max_length=30)
-    email=models.EmailField()
+    email=models.EmailField(null=True)
     profesion=models.CharField(max_length=30)
 
 class Entregable(models.Model):
