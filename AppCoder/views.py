7d61be68f8111d5584c8a7e5dd43631002e96521
karla kumako <karlakumako@gmail.com>
2024-05-24 17:50:26 -0300

    se agregan las views de acuerdo a los modelos

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -14,3 +14,18 @@ def lista_cursos(req):
    lista= Curso.objects.all()
 
    return render(req,"lista_cursos.html", {"lista_cursos":lista})
+
+def Inicio(req):
+   return HttpResponse("Pantalla inicio")
+
+def cursos(req):
+   return HttpResponse("Pantalla crusos")
+
+def estudiantes(req):
+   return HttpResponse("Pantalla estudiantes")
+
+def profesores(req):
+   return HttpResponse("Pantalla profesores")
+
+def entregables(req):
+   return HttpResponse("Pantalla entregables")
