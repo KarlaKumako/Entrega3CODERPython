cc9449ba3de29854d0ccf4bea90b0d73d140cda7
karla kumako <karlakumako@gmail.com>
2024-05-24 17:31:03 -0300

    se corrigen los signos en el codigo del views

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -9,14 +9,14 @@ class Estudiante (models.Model):
     apellido=models.CharField(max_length=30)
     email=models.EmailField()
 
-class Professor(models.Model):
-    nombre:models.CharField(max_length=30)
+class Profesor(models.Model):
+    nombre=models.CharField(max_length=30)
     apellido=models.CharField(max_length=30)
     email=models.EmailField()
     profesion=models.CharField(max_length=30)
 
 class Entregable(models.Model):
-    nombre:models.CharField(max_length=30)
+    nombre=models.CharField(max_length=30)
     fechaDeEntrega=models.DateField()
     entregado=models.BooleanField()
 
