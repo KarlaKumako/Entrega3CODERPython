edd6e5730a114b1e9d9880b61c6c5d768af7f8c7
karla kumako <karlakumako@gmail.com>
2024-05-24 18:16:31 -0300

    se crean los endpoints de los modelos

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,9 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
-from AppCoder.views import curso,lista_cursos
+from AppCoder.views import curso,lista_cursos,Inicio,cursos,profesores,estudiantes,entregables
 
 urlpatterns = [
     path('agrega-curso/<nombre>/<camada>', curso),
-    path('lista_cursos/', lista_cursos)
+    path('lista_cursos/', lista_cursos),
+    path('', lista_cursos),
+    path('cursos/', cursos),
+    path('estudiantes/', estudiantes),
+    path('profesores/', profesores),
+    path('entregables/', entregables),
 ]
